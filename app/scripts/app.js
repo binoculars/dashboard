68de7eee5a00fa7ef3ce2babd33402224ecf89e0
Barrett K. Harber <barrett.harber@gmail.com>
2016-03-20 15:31:49 -0400

    Add baseurl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+    app.baseUrl = '/dashboard/';
   }
 
   app.displayInstalledToast = function() {
